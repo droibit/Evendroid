e005f87839e9eca72b6f44d4c3be438bf8ac5144
Shinya Kumagai <roomful.rooms@gmail.com>
2014-09-11 19:27:24 +0900

    Created a startup screen
    レイアウトのみ作成した。

---
+++ app/src/main/java/com/droibit/evendroid2/fragment/StartupFragment.java
@@ -0,0 +1,12 @@
+package com.droibit.evendroid2.fragment;
+
+import android.app.Fragment;
+
+/**
+ * 初回起動時にアカウントを設定するためのフラグメント。
+ *
+ * @author kumagai
+ * @since 2014/09/11.
+ */
+public class StartupFragment extends Fragment {
+}
