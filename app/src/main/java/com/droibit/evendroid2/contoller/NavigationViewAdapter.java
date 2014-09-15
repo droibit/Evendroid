0bc8ee911bc1e875ada9f0360adaa5d697084e6f
Shinya Kumagai <roomful.rooms@gmail.com>
2014-09-15 19:01:14 +0900

    Added the Guava library

--- app/src/main/java/com/droibit/evendroid2/contoller/NavigationViewAdapter.java
+++ app/src/main/java/com/droibit/evendroid2/contoller/NavigationViewAdapter.java
@@ -2,10 +2,7 @@ package com.droibit.evendroid2.contoller;
 
 import android.content.Context;
 import android.support.annotation.DrawableRes;
-import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
